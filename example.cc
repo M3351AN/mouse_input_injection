bf56f2cd3e11eb1afc80ab36b082d654e655c1cb
渟雲 <zuiun1337@gmail.com>
2025-06-06 01:01:20 +0800

    fix: cmake file wrong
    Visual Studio generated some template, but that's not what we want/

--- example.cc
+++ example.cc
@@ -3,6 +3,7 @@
 int main() {
   // Usuage is same as mouse_event function in Windows API
   // Example usage: Click the left mouse button down (but not up)
+  printf("Clicking the left mouse button down...\n");
   my_mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
 
   system("pause");
